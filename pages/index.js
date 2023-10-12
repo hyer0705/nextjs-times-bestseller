29ac4c4fd125963384baed16f4a58544fd4d87b6
hyer0705 <hyer1205@gmail.com>
2023-10-12 17:41:11 +0900

    Add HTML markup and fetch data to Home page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,26 @@
-export default function IndexPage() {
-  return <div>Home</div>;
+import Link from "next/link";
+
+export default function IndexPage({ res: { results: bookList } }) {
+  return (
+    <main className="paper container">
+      <h2>The New York Times Best Seller Explorer.</h2>
+      <div className="collapseible-body">
+        <div className="row child-borders">
+          {bookList.map((book) => (
+            <Link href={`/list/book.list_name_encoded`}>
+              <a className="paper-btn margin">{book.display_name} &rarr;</a>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export async function getServerSideProps() {
+  const res = await (
+    await fetch("https://books-api.nomadcoders.workers.dev/lists")
+  ).json();
+
+  return { props: { res } };
 }
