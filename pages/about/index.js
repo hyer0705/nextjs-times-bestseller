09cdaf617828d553b6d6ae2e28f799c68b411b58
hyer0705 <hyer1205@gmail.com>
2023-10-12 16:57:40 +0900

    Add HTML markup to About component

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,3 +1,12 @@
 export default function About() {
-  return <h1>About Component</h1>;
+  return (
+    <main className="paper container">
+      <h1>About US</h1>
+      <p>
+        Welcome to the official explorer for The New York Times Best Seller list
+        expolorer.
+      </p>
+      <p>We hope you enjoy your stay!</p>
+    </main>
+  );
 }
