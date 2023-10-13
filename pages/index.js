4b936a0db2b0b33db204b2696c156462147270c4
hyer0705 <hyer1205@gmail.com>
2023-10-13 14:25:35 +0900

    Add title of best seller list and fetch book list data to DetailPage

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Link from "next/link";
 
-export default function IndexPage({ res: { results: bookList } }) {
+export default function IndexPage({ res: { results: bestSellerList } }) {
   return (
     <main className="paper container">
       <h2>The New York Times Best Seller Explorer.</h2>
       <div className="collapseible-body">
         <div className="row child-borders">
-          {bookList.map((book) => (
-            <Link href={`/list/book.list_name_encoded`}>
+          {bestSellerList.map((book, index) => (
+            <Link key={index} href={`/list/${book.list_name_encoded}`}>
               <a className="paper-btn margin">{book.display_name} &rarr;</a>
             </Link>
           ))}
--- pages/list/[id].js
+++ pages/list/[id].js
@@ -1,3 +1,22 @@
-export default function Detail() {
-  return <h1>Detail Component~~~</h1>;
+export default function DetailPage({ res: { results: bookList } }) {
+  const { display_name: displayName, books } = bookList || {};
+  console.log(displayName, books);
+
+  return (
+    <main className="paper">
+      <h2>{displayName}</h2>
+      <style jsx>{`
+        h2 {
+          text-align: center;
+        }
+      `}</style>
+    </main>
+  );
+}
+
+export async function getServerSideProps({ params: { id } }) {
+  const res = await (
+    await fetch(`https://books-api.nomadcoders.workers.dev/list?name=${id}`)
+  ).json();
+  return { props: { res } };
 }
