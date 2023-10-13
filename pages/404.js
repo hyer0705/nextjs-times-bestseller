79410c98eed8cddd00954288d6808c2b26586312
hyer0705 <hyer1205@gmail.com>
2023-10-13 18:12:00 +0900

    Add NotFound page

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function NotFound() {
+  return (
+    <>
+      <Head>
+        <title>Not Found | The New York Times Best Seller</title>
+      </Head>
+      <main className="paper">
+        <h1>Not Found...</h1>
+      </main>
+    </>
+  );
+}
