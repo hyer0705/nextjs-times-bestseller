37642fb7c092b6dc4d198f32a2addab6bf8f8413
hyer0705 <hyer1205@gmail.com>
2023-10-13 15:03:43 +0900

    Add HTML markup for book list to DetailPage

--- pages/list/[id].js
+++ pages/list/[id].js
@@ -1,10 +1,36 @@
 export default function DetailPage({ res: { results: bookList } }) {
   const { display_name: displayName, books } = bookList || {};
-  console.log(displayName, books);
 
   return (
     <main className="paper">
       <h2>{displayName}</h2>
+      <div className="row flex-spaces child-borders">
+        {books.map((book, index) => (
+          <div
+            key={`${index}_${book.rank}_${book.title}`}
+            className="card margin"
+            style={{ width: "330px" }}
+          >
+            <img
+              alt={book.title}
+              src={book.book_image}
+              style={{ height: "500px" }}
+            />
+            <div className="card-body">
+              <h4 className="card-title">{book.title}</h4>
+              <h5 className="card-subtitle">{book.author}</h5>
+              <p>{book.description}</p>
+              <a
+                target="_blank"
+                href={book.amazon_product_url}
+                className="paper-btn"
+              >
+                Buy now &rarr;
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
       <style jsx>{`
         h2 {
           text-align: center;
