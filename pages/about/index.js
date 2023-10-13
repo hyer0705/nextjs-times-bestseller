dde0cafe2bc9b69c1b3b0e89467c71ed737dcf80
hyer0705 <hyer1205@gmail.com>
2023-10-13 16:55:55 +0900

    Use Head component to set page title in Home, About, and Detail pages

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,12 +1,19 @@
+import Head from "next/head";
+
 export default function AboutPage() {
   return (
-    <main className="paper container">
-      <h1>About US</h1>
-      <p>
-        Welcome to the official explorer for The New York Times Best Seller list
-        expolorer.
-      </p>
-      <p>We hope you enjoy your stay!</p>
-    </main>
+    <>
+      <Head>
+        <title>About | The New York Times Best Seller</title>
+      </Head>
+      <main className="paper container">
+        <h1>About US</h1>
+        <p>
+          Welcome to the official explorer for The New York Times Best Seller
+          list expolorer.
+        </p>
+        <p>We hope you enjoy your stay!</p>
+      </main>
+    </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,25 @@
 import Link from "next/link";
+import Head from "next/head";
 
 export default function IndexPage({ res: { results: bestSellerList } }) {
   return (
-    <main className="paper container">
-      <h2>The New York Times Best Seller Explorer.</h2>
-      <div className="collapseible-body">
-        <div className="row child-borders">
-          {bestSellerList.map((book, index) => (
-            <Link key={index} href={`/list/${book.list_name_encoded}`}>
-              <a className="paper-btn margin">{book.display_name} &rarr;</a>
-            </Link>
-          ))}
+    <>
+      <Head>
+        <title>Home | The New York Times Best Seller</title>
+      </Head>
+      <main className="paper container">
+        <h2>The New York Times Best Seller Explorer.</h2>
+        <div className="collapseible-body">
+          <div className="row child-borders">
+            {bestSellerList.map((book, index) => (
+              <Link key={index} href={`/list/${book.list_name_encoded}`}>
+                <a className="paper-btn margin">{book.display_name} &rarr;</a>
+              </Link>
+            ))}
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
 
