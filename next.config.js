38563ecd1b189e859e066c8b0025197b851274fb
hyer0705 <hyer1205@gmail.com>
2023-10-18 16:33:15 +0900

    Move rewrites configuration from vercel.json to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/lists",
+        destination: "https://books-api.nomadcoders.workers.dev/lists",
+      },
+      {
+        source: "/api/list/:id",
+        destination: "https://books-api.nomadcoders.workers.dev/list?name=:id",
+      },
+    ];
+  },
 };
