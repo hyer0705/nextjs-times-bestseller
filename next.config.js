925c6b06928f6e067bd1df5e9de30345dab066ed
hyer0705 <hyer1205@gmail.com>
2023-10-18 15:12:34 +0900

    Remove unnecessary code

--- next.config.js
+++ next.config.js
@@ -1,15 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: "/api/lists",
-        destination: "https://books-api.nomadcoders.workers.dev/lists",
-      },
-      {
-        source: "/api/list/:id",
-        destination: "https://books-api.nomadcoders.workers.dev/list?name=:id",
-      },
-    ];
-  },
 };
