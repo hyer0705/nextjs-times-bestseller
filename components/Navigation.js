08dae5fd99c9e9bfd53f6479bcce8811bce378a2
hyer0705 <hyer1205@gmail.com>
2023-10-18 14:30:58 +0900

    Remove unnecessary code

--- components/Navigation.js
+++ components/Navigation.js
@@ -2,19 +2,17 @@ import Link from "next/link";
 
 export default function Navigation() {
   return (
-    <>
-      <nav className={`border fixed split-nav`}>
-        <div className="nav-brand">
-          <h4>
-            <Link href="/">Home</Link>
-          </h4>
-        </div>
-        <div className="nav-brand">
-          <h4>
-            <Link href="/about">About</Link>
-          </h4>
-        </div>
-      </nav>
-    </>
+    <nav className={`border fixed split-nav`}>
+      <div className="nav-brand">
+        <h4>
+          <Link href="/">Home</Link>
+        </h4>
+      </div>
+      <div className="nav-brand">
+        <h4>
+          <Link href="/about">About</Link>
+        </h4>
+      </div>
+    </nav>
   );
 }
